e513a97fc2b2dfc1f951c5363d47baf12c01cc4a
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-07-26 11:52:18 -0600

    almost done

--- scripts.js
+++ scripts.js
@@ -1,43 +1,45 @@
-# Custom Video Player
+//elements
 
-## Objective:
-When the HTML page is loaded in a browser, it displays a video player with controls for playing/pausing the video, scrolling through the video progress, skipping/ rewinding, changing the volume, and adjusting the playback speed. None of these controls currently work. Write the necessary JavaScript code to bring functionality to this page.
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
-## Steps:
-
-- Buttons:
-	* Play
-	* Pause
-	* Skip 
-	* Volume Slider
-	* Speed Slider
-
-``function togglePlay(){
+//build out functions
+function togglePlay(){
 	const method = video.paused ? 'play' : 'pause';
 	video[method]();
 }
+
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   console.log(icon);
   toggle.textContent = icon;
 }
+
 function skip(){
 	video.currentTime += parseFloat(this.dataset.skip);
 }
+
 function handleRangeUpdate(){
 	video[this.name] = this.value;
 }
+
 function handleProgress(){
 	const percent = (video.currentTime / video.duration) * 100;
 	progressBar.style.flexBasis = `${percent}%`;
 }
+
 function scrub(e){
 	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
 	video.currentTime = scrubTime;
-}``
+}
 
-- Event Listeners for the buttons
-	 ``video.addEventListener('click', togglePlay);
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress)
@@ -51,4 +53,4 @@ let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);``
+progress.addEventListener('mouseup', () => mousedown = false);
